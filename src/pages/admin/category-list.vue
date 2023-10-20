ae6ddf0d2ad93fed06657542e280f6278d004564
YoungZz1k <907175262@qq.com>
2023-10-20 08:48:10 +0800

    文章分类：删除功能开发

--- src/pages/admin/category-list.vue
+++ src/pages/admin/category-list.vue
@@ -56,7 +56,7 @@
                 <el-table-column prop="createTime" label="创建时间" width="180" />
                 <el-table-column label="操作">
                     <template #default="scope">
-                        <el-button type="danger" size="small">
+                        <el-button type="danger" size="small" @click="deleteCategorySubmit(scope.row)">
                             <el-icon class="mr-1">
                                 <Delete />
                             </el-icon>
@@ -83,8 +83,8 @@
 import { Search, RefreshRight } from '@element-plus/icons-vue'
 import { ref, reactive } from 'vue'
 import moment from 'moment'
-import { getCategoryPageList, addCategory } from '@/api/admin/category'
-import { showMessage } from '@/composables/util'
+import { getCategoryPageList, addCategory, deleteCategory } from '@/api/admin/category'
+import { showMessage, showModel } from '@/composables/util'
 
 // 表格数据
 const tableData = ref([])
@@ -150,6 +150,26 @@ const onSubmit = () => {
                 showMessage(message, 'error')
             }
         })
+    })
+}
+
+const deleteCategorySubmit = (row) => {
+    console.log(row.id)
+    showModel('是否确定要删除该分类？').then(() => {
+        deleteCategory(row.id).then((res) => {
+            if (res.success == true) {
+                showMessage('删除成功')
+                // 重新请求分页接口，渲染数据
+                getTableData()
+            } else {
+                // 获取服务端返回的错误消息
+                let message = res.message
+                // 提示错误消息
+                showMessage(message, 'error')
+            }
+        })
+    }).catch(() => {
+        console.log('取消了')
     })
 }
 
